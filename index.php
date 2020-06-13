3f0ebf775b6180af8955f4cf41b7b6db756f11e2
akku0225 <66835544+akku0225@users.noreply.github.com>
2020-06-13 16:50:55 +0530

    commit

---
+++ index.php
@@ -0,0 +1,25 @@
+this is my first task
+<html>
+<head>
+	<title>  form</title>
+</head>
+
+<body bgcolor="orange">
+<img scr=""   height="100px"  weight="100px>"  
+<a href="#here">go directly to the link</a>
+<h1> <b> <i><marquee bgcolor="green">RESISTRATION FORM</i></b></h1> </marquee>
+<form> 
+	
+ name:<input type="text"><br>
+  class:<input type="text"><br>
+  gender:<input type="radio" value="male" name="male"> male
+  <input type="radio" value="female" name="female">female <br>
+  password:<input type="password"><br>
+ exam:<input type="text"><br>
+   <input type="submit" value="submit" name="submit" style="height:50; width:80; fontsize:40px;">
+   	 <input type="reset" value="reset" name="reset" style="height:50; width:80; fontsize:40px;"> <br>
+   	 
+ </form>
+ 
+<a id="here"  href="baby.html">click here</a></body>
+</html>
